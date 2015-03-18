3d2902ff411759e87d8f9314699894e5b7529fd8
Eric Templeton <erictempleton1@gmail.com>
2015-03-18 08:00:17 -0400

    working on yearly and daily avg query

--- sf_crime.py
+++ sf_crime.py
@@ -133,10 +133,6 @@ per_day = db.crimes.aggregate([
     {'$sort': {'count': -1}},
     {'$limit': 10}
 ])
-"""
-
-start = datetime(2014, 1, 1, 00, 00, 00)
-end = datetime(2014, 12, 31, 23, 59, 00)
 
 avg_crimes = db.crimes.aggregate([
     {'$match':
@@ -153,14 +149,28 @@ avg_crimes = db.crimes.aggregate([
         }
     }
 ])
+"""
+
+start = datetime(2003, 1, 1, 00, 00, 00)
+end = datetime(2014, 12, 31, 23, 59, 00)
 
 total_year = db.crimes.aggregate([
+    {'$match':
+        {'Date': {'$gte': start, '$lte': end}}
+    },
     {'$group':
         {'_id': {'Year': {'$year': '$Date'}},
-         'count': {'$sum': 1}
+         'Count': {'$sum': 1},
         }
     },
-    {'$sort': {'count': -1}}
+    {'$project':
+        {'_id': 0,
+            'Year': '$_id.Year',
+            'Sum Per Year': '$Count',
+            'Avg Per Day': {'$divide': ['$Count', 365]}
+        }
+    },
+    {'$sort': {'Sum Per Year': -1}}
 ])
 
 print json.dumps(total_year, indent=4)
